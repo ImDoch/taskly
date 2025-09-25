511382d8f5ab7e36f43e574d4e076161d90838e5
Jeison Gomez <gomezres011@gmail.com>
2025-09-25 17:46:34 -0500

    Estilos iniciales de la sección principal

--- src/TasklyApp.tsx
+++ src/TasklyApp.tsx
@@ -1,10 +1,10 @@
 import { CustomHeader } from './shared/components/CustomHeader';
-import { Modal } from './shared/components/Modal';
+//import { Modal } from './shared/components/Modal';
 import { TaskList } from './taskly/components/TaskList';
 import { useTasklyUI } from './taskly/hooks/useTasklyUI';
 
 export const TasklyApp = () => {
-  const { modal, filterIsOpen, handleModal, handleFilter } = useTasklyUI();
+  const { filterIsOpen, handleModal, handleFilter } = useTasklyUI();
   return (
     <>
       <CustomHeader
